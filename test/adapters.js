266386ff4cf3c2d2d8bc3ff94f82437138c73a1c
TJ Holowaychuk <tj@vision-media.ca>
2012-12-19 12:14:52 -0800

    add subscription adapter support

---
+++ test/adapters.js
@@ -0,0 +1,31 @@
+
+var reactive = require('reactive');
+var domify = require('component-domify');
+var assert = require('component-assert');
+
+var user = {};
+
+user.bind = function(event, fn){
+  this._fn = fn;
+};
+
+user.trigger = function(event){
+  this._fn();
+};
+
+describe('reactive.subscribe(fn)', function(){
+  it('should define a subscription method', function(){
+    reactive.subscribe(function(obj, prop, fn){
+      obj.bind('change ' + prop, fn);
+    });
+
+    reactive.unsubscribe(function(obj, prop, fn){
+      obj.unbind('change ' + prop);
+    });
+
+    var el = domify('<div><h1 data-text="name"></h1></div>')[0];
+    reactive(el, user);
+    user.name = 'Tobi';
+    user.trigger('change name');
+  })
+})
