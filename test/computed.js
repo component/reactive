9463f06608397ff5099dfe48463725392c7d361d
TJ Holowaychuk <tj@vision-media.ca>
2013-02-25 14:14:38 -0800

    add computed properties support
    via 'fullname < first last'

---
+++ test/computed.js
@@ -0,0 +1,63 @@
+
+var reactive = require('reactive');
+var Emitter = require('component-emitter');
+var domify = require('component-domify');
+var assert = require('component-assert');
+
+function User(first, last) {
+  this.first = first;
+  this.last = last;
+}
+
+Emitter(User.prototype);
+
+User.prototype.fullname = function(){
+  return this.first + ' ' + this.last;
+};
+
+User.prototype.removed = function(){
+  return !! this.removed_at;
+};
+
+var user = new User('Tobi', 'Ferret');
+
+describe('computed properties', function(){
+  it('should work with multiple properties', function(){
+    var el = domify('<p><em data-text="fullname < first last"></em></p>')[0];
+    var user = new User('Tobi', 'Ferret');
+    var view = reactive(el, user);
+    var em = el.children[0];
+
+    assert('Tobi Ferret' == em.textContent);
+  })
+
+  it('should react to changes', function(){
+    var el = domify('<p><em data-text="fullname < first last"></em></p>')[0];
+    var user = new User('Tobi', 'Ferret');
+    var view = reactive(el, user);
+    var em = el.children[0];
+
+    assert('Tobi Ferret' == em.textContent);
+
+    user.first = 'Loki';
+    user.emit('change first');
+    assert('Loki Ferret' == em.textContent);
+
+    user.last = 'Holowaychuk';
+    user.emit('change last');
+    assert('Loki Holowaychuk' == em.textContent);
+  })
+
+  it('should work with .value() only', function(){
+    var el = domify('<p><em data-hide="removed < removed_at"></em></p>')[0];
+    var user = new User('Tobi', 'Ferret');
+    var view = reactive(el, user);
+    var em = el.children[0];
+
+    assert('show' == em.className);
+
+    user.removed_at = new Date;
+    user.emit('change removed_at');
+    assert('hide' == em.className);
+  })
+})
